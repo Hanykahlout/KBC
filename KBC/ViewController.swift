e9fa0a21d62a8129afdfc4cf9216b847f48f257b
Hany Alkahlout <hanykahlout@gmail.com>
2024-10-09 14:34:25 +0400

    Initial Commit

---
+++ KBC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KBC
+//
+//  Created by Hany Alkahlout on 10/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
